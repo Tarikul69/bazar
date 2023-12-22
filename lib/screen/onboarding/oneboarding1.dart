693034c3d9fb0e0cb7f3363f8376efdcdab6cf5e
Tarikul69 <tarikulabir69@gmail.com>
2023-12-22 22:41:20 +0600

    Onboarding

--- lib/screen/onboarding/oneboarding1.dart
+++ lib/screen/onboarding/oneboarding1.dart
@@ -4,6 +4,24 @@ class oneboarding1 extends StatelessWidget {
   const oneboarding1({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "Skip",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
