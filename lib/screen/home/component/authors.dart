82dfbd8918e36cb32b77d0825939871c7c6f6eb7
Tarikul69 <tarikulabir69@gmail.com>
2023-12-25 16:37:30 +0600

    Author/Vendords

--- lib/screen/home/component/authors.dart
+++ lib/screen/home/component/authors.dart
@@ -5,41 +5,79 @@ class authors extends StatelessWidget {
   const authors({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        foregroundColor: Colors.black,
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Text(
-          "Authors",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text(
+            "Authors",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            ListTile(
-              subtitle: Text("Check the author"),
-              title: Text(
-                "Author",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.deepPurple,
-                ),
-              ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
             )
           ],
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              ListTile(
+                subtitle: Text("Check the author"),
+                title: Text(
+                  "Author",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ),
+              Container(
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(80),
+                  ),
+                ),
+                child: TabBar(
+                  labelColor: Colors.black,
+                  tabs: [
+                    Tab(
+                      text: "All",
+                    ),
+                    Tab(
+                      text: "Poets",
+                    ),
+                    Tab(
+                      text: "Playwrights",
+                    ),
+                    Tab(
+                      text: "Novelist",
+                    ),
+                    Tab(
+                      text: "Journal",
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Expanded(
+                  child: TabBarView(
+                children: [],
+              ))
+            ],
+          ),
         ),
       ),
     );
--- lib/screen/home/component/vendor.dart
+++ lib/screen/home/component/vendor.dart
@@ -1,29 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class vendor extends StatelessWidget {
   const vendor({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        elevation: 0,
-        //leading: BackButton(),
-        centerTitle: true,
-        title: Text(
-          "Vendords",
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text(
+            "Vendords",
+            style: TextStyle(
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+            )
+          ],
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              ListTile(
+                subtitle: Text("Check the author"),
+                title: Text(
+                  "Author",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ),
+              Container(
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(80),
+                  ),
+                ),
+                child: TabBar(
+                  labelColor: Colors.black,
+                  tabs: [
+                    Tab(
+                      text: "All",
+                    ),
+                    Tab(
+                      text: "Poets",
+                    ),
+                    Tab(
+                      text: "Playwrights",
+                    ),
+                    Tab(
+                      text: "Novelist",
+                    ),
+                    Tab(
+                      text: "Journal",
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Expanded(
+                  child: TabBarView(
+                children: [],
+              ))
+            ],
           ),
         ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
-          )
-        ],
       ),
     );
   }
