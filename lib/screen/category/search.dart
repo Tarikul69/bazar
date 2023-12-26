bf9ba5f9d9aeba4e29dc51491b935da41d32cd69
Tarikul69 <tarikulabir69@gmail.com>
2023-12-26 21:06:02 +0600

    Category Search

--- lib/screen/category/search.dart
+++ lib/screen/category/search.dart
@@ -19,11 +19,48 @@ class search extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.all(15),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextFormField(
+            TextField(
               decoration: InputDecoration(
-                icon: FaIcon(Icons.search),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                // icon: FaIcon(Icons.search),
                 hintText: "Search",
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Title(
+              color: Colors.black,
+              child: Text(
+                "Rescent Searches",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "The Good Sister",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            Divider(
+              color: Colors.grey,
+            ),
+            Text(
+              "Carries Fisher",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
               ),
             )
           ],
