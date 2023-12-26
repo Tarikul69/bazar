d635fe3e89789b853bf61db5c47a9fe4e151d3e2
Tarikul69 <tarikulabir69@gmail.com>
2023-12-26 19:15:08 +0600

    Server

---
+++ lib/server/server.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class server extends StatelessWidget {
+  const server({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Server"),
+      ),
+    );
+  }
+}
