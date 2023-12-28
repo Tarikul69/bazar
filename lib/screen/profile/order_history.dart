368c53bf36b8a7f068454fca47dc025a402ffb48
Tarikul69 <tarikulabir69@gmail.com>
2023-12-29 01:22:41 +0600

    Update Profile

--- lib/screen/profile/order_history.dart
+++ lib/screen/profile/order_history.dart
@@ -17,7 +17,27 @@ class order_history extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Container(),
+      body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 2,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(10, (index) {
+          return SizedBox(
+            child: Card(
+              shape: Border.all(),
+              color: Colors.purple,
+              child: Text(
+                'Item $index',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
     );
   }
 }
