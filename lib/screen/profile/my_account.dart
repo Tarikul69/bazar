5b96477883c646fd92e736fb3e7a3cd256708b9d
Tarikul69 <tarikulabir69@gmail.com>
2023-12-29 14:28:53 +0600

    My Account

--- lib/screen/profile/my_account.dart
+++ lib/screen/profile/my_account.dart
@@ -19,24 +19,96 @@ class my_account extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.all(10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Stack(
               alignment: Alignment.topCenter,
               children: [
                 Container(
-                  color: Color.fromARGB(255, 203, 185, 206),
+                  color: Color.fromARGB(255, 154, 119, 160),
                 ),
                 CircleAvatar(
                   foregroundColor: Colors.grey,
                   backgroundColor: Colors.grey,
                   radius: 50,
                 ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.camera_alt_outlined),
-                )
+                // IconButton(
+                //   onPressed: () {},
+                //   icon: Icon(Icons.camera_alt_outlined),
+                // )
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Change Picture",
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                      ),
+                    ))
               ],
-            )
+            ),
+            Title(
+              color: Colors.black,
+              child: Text(
+                "Name",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(6),
+                    ),
+                  ),
+                  hintText: 'Name'),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Title(
+              color: Colors.black,
+              child: Text(
+                "Email",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(6),
+                    ),
+                  ),
+                  hintText: 'Email'),
+              validator: (String? value) {
+                return;
+                (value != null && value.contains('@'));
+              },
+            ),
+            Title(
+              color: Colors.black,
+              child: Text(
+                "Phone Number",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(6),
+                  ),
+                ),
+                hintText: 'Phone Number',
+                icon: Icon(Icons.phone),
+              ),
+            ),
           ],
         ),
       ),
