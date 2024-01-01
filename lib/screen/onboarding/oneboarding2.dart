35d9624c0621b0f9c9f2afaa048ae08959350bac
Tarikul69 <tarikulabir69@gmail.com>
2024-01-01 21:15:36 +0600

    Update

---
+++ lib/screen/onboarding/oneboarding2.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class oneboarding2 extends StatelessWidget {
+  const oneboarding2({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
