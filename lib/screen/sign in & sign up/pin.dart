6fc148d167bc1ea2c51f5e1a284a180fdd213ccd
Tarikul69 <tarikulabir69@gmail.com>
2024-01-03 18:11:11 +0600

    Pin Verification

---
+++ lib/screen/sign in & sign up/pin.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class pin extends StatelessWidget {
+  const pin({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+      ),
+      body: Container(),
+    );
+  }
+}
--- lib/screen/sign in & sign up/verification.dart
+++ lib/screen/sign in & sign up/verification.dart
@@ -1,4 +1,7 @@
+import 'package:bazar/screen/sign%20in%20&%20sign%20up/pin.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class verification extends StatelessWidget {
   const verification({Key? key}) : super(key: key);
@@ -13,7 +16,48 @@ class verification extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.all(20),
         child: Column(
-          children: [],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ListTile(
+              title: Text(
+                "Verification",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+              subtitle: Text(""),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter your phone or email",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Get.to(pin());
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurple,
+                fixedSize: Size(Get.width, 60),
+              ),
+              child: Text(
+                "Send",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
