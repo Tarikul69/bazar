1b68787103b073d68cc8e8cabf82d922fdd6027e
Tarikul69 <tarikulabir69@gmail.com>
2024-01-04 14:13:38 +0600

    Review

---
+++ lib/screen/order status/review.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class review extends StatelessWidget {
+  const review({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
